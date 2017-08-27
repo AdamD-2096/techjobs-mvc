5517b20f8340cd18080fbec2bed2726c06aa393e
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-08-26 18:05:51 -0700

    Assignment completed (sorry for not committing well, until it was done it was in a transitional period that didn't seem fit to commit)

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -18,8 +19,28 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
+        model.addAttribute("col","all");
+        model.addAttribute("term","");
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
+    }
+
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("col",searchType);
+        model.addAttribute("term",searchTerm);
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "All " + ListController.columnChoices.get(searchType) + " Values");
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
     }
 
     // TODO #1 - Create handler to process search request and display results
